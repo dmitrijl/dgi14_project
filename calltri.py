fa6e8ef9ec8e7af562a698c594e82fd764d751d6
Dmitrij Lioubartsev <tinker234@hotmail.com>
2015-05-09 13:04:24 +0200

    pushing calltri

--- calltri.py
+++ calltri.py
@@ -1,15 +1,19 @@
 
 from laspy import file
+import os
+import subprocess
 
-if __name__ == "__main__":
-    lasf = file.File("a.las", mode = "r")
+def create_poly_file(lasfilename, v_c = -1):
+    lasf = file.File(lasfilename, mode = "r")
     v_count = len(lasf)
     x = lasf.get_x_scaled()
     y = lasf.get_y_scaled()
     z = lasf.get_z_scaled()
     
-    v_count = 10 #remove this line later
-    pfile = open("a.poly", "w")
+    #v_count = 10000 #remove this line later
+    if v_c > 0:
+        v_count = v_c
+    pfile = open("%s.poly"%os.path.splitext(lasfilename)[0], "w")
     pfile.write("%i 2 1 0\n" % v_count)
     for i in xrange(v_count):
         pfile.write("%i %s %s %s\n" % (i+1,x[i],y[i],z[i]))
@@ -17,7 +21,48 @@ if __name__ == "__main__":
     pfile.write("0 0\n")
     pfile.write("0\n")
     pfile.close()
-    lasf.close()
+    lasf.close() 
+
+def combined_polys(filenames):
+    readers = []
+    for f in filenames:
+        readers.append(file.File(f,mode = "r"))
+        
+    total_v = 0
+    for r in readers:
+        total_v += len(r)
+        
+    pfile = open("full.poly", "w")
+    pfile.write("%i 2 1 0\n"%total_v)
+    i = 1
+    for k,r in enumerate(readers):
+        x = lasf.get_x_scaled()
+        y = lasf.get_y_scaled()
+        z = lasf.get_z_scaled()
+        c = len(r)
+        for j in xrange(c):
+            pfile.write("%i %s %s %s\n" % (i,x[j],y[j],z[j]))
+            i += 1
+        print "file nr %i done"%k
+        
+    pfile.write("0 0\n")
+    pfile.write("0\n")
+    
+    for r in readers:
+        r.close()
+    pfile.close()
+
+def triangulate(f):
+    s = subprocess.check_output(["tri.exe","-p",f])
+    print s
+    
+if __name__ == "__main__":
+    create_poly_file("a.las",10000)
+    #create_poly_file("b.las")
+    #create_poly_file("c.las")
+    #create_poly_file("d.las")
+    print "poly file created, triangulate now"
+    triangulate("a")
     
     
      
