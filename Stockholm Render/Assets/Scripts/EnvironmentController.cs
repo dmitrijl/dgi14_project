4dab1efdd7905eb9fa842b90100c5d12d2aba336
kricka91 <kristoffer.hallqvist@gmail.com>
2015-05-29 18:25:39 +0200

    Animated water, interface and ready to build.

---
+++ Stockholm Render/Assets/Scripts/EnvironmentController.cs
@@ -0,0 +1,286 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class EnvironmentController : MonoBehaviour {
+
+	public GameObject water, plane;
+	public bool colorize;
+	public bool flattenBuildings;
+
+	public enum BuildingDetection{None, AbsoluteHeight, NeighborHeight};
+	public BuildingDetection buildingDetection;
+	public float heightLimit;
+	public bool spikeRemoval;
+
+	public readonly float updateSpeed = 2f;
+	public readonly float shiftSpeedIncreaseFactor = 4;
+
+	private List<int>[] nbList;
+	private Mesh mesh;
+	private Vector3[] vertices;
+	private Vector2[] UV;
+	private int[] triangles;
+	private int[] originalClassifications;
+	private int[] classifications;
+	private float[] orignialHeights;
+	private Color32[] colors;
+
+
+	//Converts the triangles into edge lists (each triangle consisting of three edges)
+	void trianglesToEdges(int nrVert, ref int[] t, out List<int>[] nbl) {
+		//Convert triangles to edges. Each triangle is three edges
+		nbl = new List<int>[nrVert];
+		for (int i = 0;i<nrVert;++i) {
+			nbl[i] = new List<int>();
+		}
+		int n1, n2;
+		for (int i = 0;i<t.Length;i+=3) {
+			for (int j = 0;j<3;++j) {
+				for (int k = 0;k<3;++k) {
+					if (j == k) continue;
+					n1 = t[i+j];
+					n2 = t[i+k];
+					nbl[n1].Add(n2);
+				}
+			}
+		}
+	}
+	
+	//Lower the y value of all water classified nodes
+	void lowerWater(ref Vector3[] v, ref int[] c) {
+		for (int i = 0;i<v.Length;++i) {
+			if (c[i] == MeshReader.Classification.Water) {
+				v[i].y = -1.0f;
+			}
+		}
+	}
+	
+	//Remove big spikes
+	void removeSpikes(ref Vector3[] v, ref List<int>[] nbLists, float hl, ref int[] c) {
+		for (int n1 = 0;n1<nbLists.Length;++n1) {
+			for (int j = 0;j<nbLists[n1].Count;++j) {
+				int n2 = nbLists[n1][j];
+				if (v[n1].y > hl) {
+					v[n1].y = v[n2].y;
+				}
+			}
+		}
+	}
+	
+	//Detects building using the neighbor height method
+	void detectBuildingsNH(ref Vector3[] v, ref List<int>[] nbLists, float hl, ref int[] c) {
+		//Look for great height differences between neighboring vertices
+		int n1, n2;
+		float avgH;
+		int nrDiffs;
+		int changes;
+		for (int i = 0;i<1;++i) {
+			changes = 0;
+			for (n1 = 0; n1<nbLists.Length; ++n1) {
+				avgH = 0.0f;
+				nrDiffs = 0;
+				for (int j = 0; j<nbLists[n1].Count; ++j) {
+					n2 = nbLists[n1][j];
+					
+					if (v[n1].y - v[n2].y > hl && (c[n2] != MeshReader.Classification.Water || 
+					                                       c[n2] != MeshReader.Classification.Unclassified) && 
+					                                      (c[n1] == MeshReader.Classification.Building || 
+					                                       c[n1] == MeshReader.Classification.Ground) ) {
+						avgH += v[n2].y;
+						++nrDiffs;
+					}
+				}
+				if (avgH > 0.0f) {
+					avgH /= nrDiffs;
+					v[n1].y = avgH;
+					c[n1] = MeshReader.Classification.Building;
+					++changes;
+				}
+			}
+			//Debug.Log("Iteration " + (i+1) + ": Number of changes: " + changes);
+		}
+				
+	}
+	
+	//Detects building using the absolute height method
+	void detectBuildingsAH(ref Vector3[] v, float hl, ref int[] c) {
+		for (int j = 0; j < v.Length; ++j) {
+			if (v[j].y > hl) {
+				c[j] = MeshReader.Classification.Building;	//High point
+				v[j].y = hl;
+			}
+		}
+	}
+	
+
+
+
+	//Update the mesh
+	void updateMesh() {
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		Vector2[] uvs = new Vector2[vertices.Length];
+		for (int i=0; i < uvs.Length; i++) {
+			uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+		}
+		mesh.uv = uvs;
+
+		//Color the vertices
+		if (colorize) {
+			for (int j = 0; j < vertices.Length; ++j) {
+				colors [j] = MeshReader.Classification.intToColor32 (classifications [j]);
+			}
+		} else {
+			for (int j = 0; j < vertices.Length; ++j) {
+				colors [j] = (Color32)Color.white;
+			}
+		}
+		mesh.colors32 = colors;
+	}
+
+
+	//Process the model, apply building detection, etc...
+	void processModel(BuildingDetection bd) {
+		//Clone orignialHeights and classifications first
+		for (int i = 0;i<vertices.Length;++i) {
+			vertices[i].y = orignialHeights[i];
+			classifications[i] = originalClassifications[i];
+		}
+
+		if (spikeRemoval) {
+			removeSpikes (ref vertices, ref nbList, 50.0f, ref classifications);
+		}
+
+		if (bd == BuildingDetection.None) return;
+
+		//Select algorithm
+		if (buildingDetection == BuildingDetection.NeighborHeight) {
+			detectBuildingsNH(ref vertices, ref nbList, heightLimit, ref classifications);
+		} else if (buildingDetection == BuildingDetection.AbsoluteHeight) {
+			detectBuildingsAH(ref vertices, heightLimit, ref classifications);
+		}
+
+		//Restore orignialHeights or use the new ones
+		float diff = 0.0f;
+		int nrDiffs = 0;
+		if (!flattenBuildings) {
+			for (int i = 0;i<orignialHeights.Length;++i) {
+				if (vertices[i].y != orignialHeights[i]) {
+					diff += Math.Abs(vertices[i].y - orignialHeights[i]);
+					++nrDiffs;
+				}
+				vertices[i].y = orignialHeights[i];
+			}
+		}
+		//Debug.Log("Average diff: " + diff/nrDiffs);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+		mesh = plane.GetComponent<MeshFilter>().mesh;
+		vertices = mesh.vertices;
+		UV = mesh.uv;
+		triangles = mesh.triangles;
+		colors = mesh.colors32;
+		classifications = new int[vertices.Length];
+		originalClassifications = new int[classifications.Length];
+		orignialHeights = new float[vertices.Length];
+
+		//Extract classifications information from colors
+		for (int i = 0; i < vertices.Length; ++i) {
+			originalClassifications[i] = MeshReader.Classification.color32ToInt((Color32)colors[i]);
+			orignialHeights[i] = vertices[i].y;
+		}
+
+		//Create an edge list for building and spike detection and eventually other things
+		trianglesToEdges(vertices.Length, ref triangles, out nbList);
+		processModel(buildingDetection);
+		if (spikeRemoval) {
+			removeSpikes (ref vertices, ref nbList, 50.0f, ref classifications);
+		}
+
+		//Apply changes
+		//mesh = new Mesh();
+		//plane.GetComponent<MeshFilter>().mesh = mesh;
+		updateMesh();
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		float us = updateSpeed;
+		if (Input.GetKey (KeyCode.LeftShift) == true) {
+			us *= shiftSpeedIncreaseFactor;
+		}
+		float uc = us * Time.deltaTime;
+
+		if(Input.GetKeyDown (KeyCode.Alpha1)) {
+			//Toggle static water
+			water.SetActive(!water.activeSelf);
+		}
+
+		if(Input.GetKey (KeyCode.Alpha2)) {
+			//Decrease the water level
+			water.transform.Translate(Vector3.down*uc);
+		}
+
+		if(Input.GetKey (KeyCode.Alpha3)) {
+			//Increase the water level
+			water.transform.Translate(Vector3.up*uc);
+		}
+
+		if(Input.GetKeyDown (KeyCode.Alpha4)) {
+			//Toggle building flattening
+			flattenBuildings = !flattenBuildings;
+			processModel(buildingDetection);
+		}
+		
+		if(Input.GetKeyDown (KeyCode.Alpha5)) {
+			//Use no building detection
+			buildingDetection = BuildingDetection.None;
+			processModel(buildingDetection);
+		}
+		
+		if(Input.GetKeyDown (KeyCode.Alpha6)) {
+			//Use absolute height building detection
+			buildingDetection = BuildingDetection.AbsoluteHeight;
+			processModel(buildingDetection);
+		}
+
+		if(Input.GetKeyDown (KeyCode.Alpha7)) {
+			//Use neighbor height building detection
+			buildingDetection = BuildingDetection.NeighborHeight;
+			processModel(buildingDetection);
+		}
+		
+		if(Input.GetKey (KeyCode.Alpha8)) {
+			//Decrease height limit for building detection
+			heightLimit -= uc;
+			processModel(buildingDetection);
+		}
+		
+		if(Input.GetKey (KeyCode.Alpha9)) {
+			//Increase height limit for building detection
+			heightLimit += uc;
+			processModel(buildingDetection);
+		}
+
+		if(Input.GetKeyDown (KeyCode.Alpha0)) {
+			//Toggle spike removal
+			spikeRemoval = !spikeRemoval;
+			processModel(buildingDetection);
+		}
+
+		if(Input.GetKey (KeyCode.Escape)) {
+			//Exit program
+			Application.Quit();
+		}
+
+		updateMesh();
+	}
+}
---
+++ Stockholm Render/Assets/Scripts/MeshReader.cs
@@ -0,0 +1,359 @@
+﻿using UnityEngine;
+//using UnityEditor;	//Must be commented when building
+using System.Collections;
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+public class MeshReader : MonoBehaviour {
+
+	public GameObject plane;
+	public string filesetName;
+	public string fileName;
+	public float x_base;
+	public float z_base;
+	public bool save_as_asset;
+	public bool read_mesh;
+
+
+	public static class Classification {
+		public const int Unclassified=1;
+		public const int Ground=2;
+		public const int Water=9;
+		public const int Bridge=11;
+		public const int Building=21;
+		public const int Undefined=-1;
+
+		public static readonly Color32 cUnclassified=(Color32)Color.red;
+		public static readonly Color32 cGround=(Color32)Color.grey;
+		public static readonly Color32 cWater=(Color32)Color.Lerp(Color.blue, Color.cyan, 0.2f);
+		public static readonly Color32 cBridge=(Color32)Color.Lerp(Color.grey, Color.white, 0.5f);
+		public static readonly Color32 cBuilding=(Color32)Color.magenta;
+		public static readonly Color32 cUndefined=(Color32)Color.yellow;
+
+		public static Color32 intToColor32(int ic) {
+			switch (ic) {
+			case Unclassified: return cUnclassified;
+			case Ground: return cGround;
+			case Water: return cWater;
+			case Bridge: return cBridge;
+			case Building: return cBuilding;
+			default: return cUndefined;
+			}
+		}
+
+		public static int color32ToInt(Color32 c32) {
+			if (c32.Equals(cUnclassified)) return Unclassified;
+			if (c32.Equals(cGround)) return Ground;
+			if (c32.Equals(cWater)) return Water;
+			if (c32.Equals(cBridge)) return Bridge;
+			if (c32.Equals(cBuilding)) return Building;
+			return Undefined;
+		}
+
+	}
+
+	private Mesh mesh;
+	private string dir_path;
+	private bool sepNodeFile;
+	private string[] words;
+	private Char[] delim = {' '};
+	private int nrVert, nrAttr, nrBM;
+	//private int dim;	//Must be 2. Not currently used, though.
+
+	private Vector3[] vertices;
+	private Vector2[] UV;
+	private int[] triangles;
+	private int[] classifications;
+	
+
+	//Prints the vertices
+	void debugVertices() {
+		Debug.Log("The vertices are:\n");
+		Debug.Log(vertices);
+		foreach (Vector3 v3 in vertices) {
+			Debug.Log(v3.x);
+			Debug.Log(v3.y);
+			Debug.Log(v3.z);
+		}
+	}
+
+	//Prints the triangles
+	void debugTriangles() {
+		Debug.Log("The triangles are:\n");
+		Debug.Log(triangles);
+		for (int i = 0; i<triangles.Length; ++i) {
+			Debug.Log(triangles[i]);
+		}
+	}
+
+
+	//Reads and parses a .poly file
+	void readPolyFile(string fPath) {
+		Debug.Log("Reading POLY file!");
+		StreamReader inp_stm = new StreamReader(fPath);
+		string inp_ln = inp_stm.ReadLine( );	//Read line about vertices
+		words = inp_ln.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+		int.TryParse(words[0], out nrVert);
+		//int.TryParse(words[1], out dim);
+		int.TryParse(words[2], out nrAttr);
+		--nrAttr;	//To compensate for the fact that height is an attribute
+		int.TryParse(words[3], out nrBM);
+		
+		vertices = new Vector3[nrVert];
+		if (nrVert == 0) {
+			sepNodeFile = true;	//Separate node file exists
+		} else {
+			classifications = new int[nrVert];
+			for (int i = 0; i<nrVert; ++i) {
+				inp_ln = inp_stm.ReadLine ();	//Read vertex line
+				words = inp_ln.Split (delim, StringSplitOptions.RemoveEmptyEntries);
+				float.TryParse (words [1], out vertices [i].x);
+				float.TryParse (words [2], out vertices [i].y);
+				float.TryParse (words [3], out vertices [i].z);
+				float attr, bm;	//Dummy placeholders for now
+				for (int k = 4; k<4+nrAttr; ++k) {
+					float.TryParse (words [k], out attr);
+					classifications[i] = (int)attr;
+				}
+				for (int k = 4+nrAttr; k<4+nrAttr+nrBM; ++k) {
+					float.TryParse (words [k], out bm);
+				}
+			}
+		}
+		
+		//Read line about segments
+		inp_ln = inp_stm.ReadLine( );
+		words = inp_ln.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+		int nrSegments;
+		int.TryParse (words [0], out nrSegments);
+		
+		for (int i = 0;i < nrSegments; ++i) {
+			inp_ln = inp_stm.ReadLine( );
+			//TODO parse segments
+		}
+		
+		//Read line about holes
+		inp_ln = inp_stm.ReadLine( );
+		words = inp_ln.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+		int nrHoles;
+		int.TryParse (words [0], out nrHoles);
+		
+		for (int i = 0;i < nrHoles; ++i) {
+			inp_ln = inp_stm.ReadLine( );
+			//TODO parse holes
+		}
+	}
+
+	
+	//Reads and parses a .node file
+	void readNodeFile(string fPath) {
+		Debug.Log("Reading NODE file!");
+		StreamReader inp_stm = new StreamReader(fPath);
+		string inp_ln = inp_stm.ReadLine( );	//Read line about vertices
+		words = inp_ln.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+		int.TryParse(words[0], out nrVert);
+		//int.TryParse(words[1], out dim);
+		int.TryParse(words[2], out nrAttr);
+		--nrAttr;	//To compensate for the fact that height is an attribute
+		int.TryParse(words[3], out nrBM);
+		
+		vertices = new Vector3[nrVert];
+		classifications = new int[nrVert];
+		
+		for (int i = 0;i<nrVert;++i) {
+			inp_ln = inp_stm.ReadLine( );	//Read vertex line
+			words = inp_ln.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+			float.TryParse(words[1], out vertices[i].x);
+			float.TryParse(words[2], out vertices[i].z);
+			float.TryParse(words[3], out vertices[i].y);
+			float attr, bm;	//Dummy placeholders for now
+			for (int k = 4;k<4+nrAttr;++k) {
+				float.TryParse(words[k], out attr);
+				classifications[i] = (int)attr;
+			}
+			for (int k = 4+nrAttr;k<4+nrAttr+nrBM;++k) {
+				float.TryParse(words[k], out bm);
+			}
+		}
+		inp_stm.Close( );
+	}
+
+
+	//Reads and parses a .ele file
+	void readEleFile(string fPath) {
+		Debug.Log("Reading ELE file!");
+		StreamReader inp_stm = new StreamReader(fPath);
+		string inp_ln = inp_stm.ReadLine ();
+		string[] meta = inp_ln.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+		if (meta.Length != 3) {
+			Debug.Log("First line has more than 3 entries...");
+		}
+		int numTriangles =  Convert.ToInt32(meta [0]);
+		int triangleNumVertices = Convert.ToInt32 (meta [1]);
+		if (triangleNumVertices != 3) {
+			Debug.Log("Not 3 vertices in a triangle!");
+		}
+		
+		ArrayList triArray = new ArrayList (numTriangles*3);
+		for(int i = 0;i < numTriangles; i++) {
+			inp_ln = inp_stm.ReadLine( );
+			string[] tokens = inp_ln.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+			triArray.Add(Convert.ToInt32(tokens[1])-1);
+			triArray.Add(Convert.ToInt32(tokens[2])-1);
+			triArray.Add(Convert.ToInt32(tokens[3])-1);
+		}
+		
+		triangles = (int[])triArray.ToArray (typeof(int));
+		inp_stm.Close( );
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		//Debug.Log("Running script!!!\n");
+		if (!read_mesh) {
+			Debug.Log("Skipped reading mesh from file.\n");
+			Debug.Log("Loading from asset database...\n");
+			//mesh = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Stadsholmen/res.asset", typeof(Mesh)) as Mesh;
+			//plane.GetComponent<MeshFilter>().mesh = mesh;
+			return;
+		}
+
+		dir_path = "Assets/polyfiles/";	//Change as appropriate
+		UV = new Vector2[0];
+		classifications = new int[0];
+		sepNodeFile = false;
+		string polyname = filesetName + "/" + fileName;
+		StreamReader inp_stm;
+
+		//Read poly file
+		readPolyFile(dir_path + polyname + ".1.poly");
+
+		//Read node file (if necessary)
+		if (sepNodeFile) {
+			readNodeFile(dir_path + polyname + ".1.node");
+		}
+
+		//Read ele file (triangles)
+		readEleFile(dir_path + polyname + ".1.ele");
+
+
+		//Debug vertices and triangles
+		//debugVertices();
+		//debugTriangles();
+
+
+		float verticesMinX = 9999999999.0f;
+		float verticesMaxX = -99999999999.0f;
+		float verticesMinZ = 9999999999.0f;
+		float verticesMaxZ = -99999999999.0f;
+		float newmin = 0f;
+		float newmax = 100f;
+		for (int i = 0; i < vertices.Length; i++) {
+			if(vertices[i].x < verticesMinX) {
+				verticesMinX = vertices[i].x;
+			} else if (vertices[i].x > verticesMaxX) {
+				verticesMaxX = vertices[i].x;
+			}
+
+			if(vertices[i].z < verticesMinZ) {
+				verticesMinZ = vertices[i].z;
+			} else if (vertices[i].z > verticesMaxZ) {
+				verticesMaxZ = vertices[i].z;
+			}
+		}
+
+		for (int i = 0; i < vertices.Length; i++) {
+			//vertices[i].x = ((vertices[i].x - verticesMinX)*newmax)/(verticesMaxX-verticesMinX);
+			//vertices[i].z = ((vertices[i].z - verticesMinZ)*newmax)/(verticesMaxZ-verticesMinZ);
+			vertices[i].x = (vertices[i].x - verticesMinX) + x_base;
+			vertices[i].z = (vertices[i].z - verticesMinZ) + z_base;
+			//vertices[i].y = -vertices[i].y;
+		}
+
+		//for (int i = 0; i < vertices.Length; i++) {
+		//	vertices[i].x = - vertices[i].x;
+		//}
+
+		Debug.Log ("Max X: " + (verticesMaxX - verticesMinX));
+		Debug.Log ("Max Z: " + (verticesMaxZ - verticesMinZ));
+		/*
+		Array.Reverse (vertices);
+		int l = vertices.Length-1;
+		for (int i = 0; i < triangles.Length; i++) {
+			triangles[i] = l-triangles[i];
+		}
+		*/
+
+
+
+
+
+		mesh = new Mesh();
+		plane.GetComponent<MeshFilter>().mesh = mesh;
+		mesh.vertices = vertices;
+		//mesh.uv = UV;
+		Array.Reverse (triangles);
+		mesh.triangles = triangles;
+
+		Vector2[] uvs = new Vector2[vertices.Length];
+		
+		for (int i=0; i < uvs.Length; i++) {
+			uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+		}
+		mesh.uv = uvs;
+
+
+		//Color the vertices
+		Color32[] colors = new Color32[vertices.Length];
+		for (int j = 0; j < vertices.Length; ++j) {
+			colors[j] = Classification.intToColor32(classifications[j]);
+		}
+		mesh.colors32 = colors;
+		
+		
+		Debug.Log ("numer of vertices: " + mesh.vertices.Length);
+		Debug.Log ("number of triangles: " + (mesh.triangles.Length/3.0f));
+		Debug.Log ("number of normals: " + mesh.normals.Length);
+		mesh.RecalculateNormals ();
+		mesh.RecalculateBounds ();
+		Debug.Log ("numer of vertices after recalculation: " + mesh.vertices.Length);
+		Debug.Log ("number of triangles after recalculation: " + (mesh.triangles.Length/3.0f));
+		Debug.Log ("number of normals after recalculation: " + mesh.normals.Length);
+		/*for (int i = 0; i < vertices.Length; i++) {
+			vertices[i].x = - vertices[i].x;
+		}*/
+		/*
+		Vector3[] normals = mesh.normals;
+		for (int i = 0; i < normals.Length; i++) {
+			normals[i] = -normals[i];
+		}
+		mesh.normals = normals;
+
+		*/
+
+
+		/*
+		//Must be commented when building
+		if (save_as_asset) {
+			AssetDatabase.CreateFolder("Assets/Prefabs", filesetName);
+			AssetDatabase.CreateAsset(mesh, "Assets/Prefabs/" + polyname + ".asset");
+			AssetDatabase.SaveAssets();
+
+			//PrefabUtility.CreatePrefab (dir_path + filesetName + ".prefab",plane);
+			//plane.GetComponent<MeshFilter>().mesh = mesh;
+
+			UnityEngine.Object prefab = EditorUtility.CreateEmptyPrefab("Assets/Prefabs/" + polyname + ".prefab");
+			EditorUtility.ReplacePrefab(plane, prefab, ReplacePrefabOptions.ReplaceNameBased);
+		}
+		*/
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
